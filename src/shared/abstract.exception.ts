21f038405cd15fed1e8c747d7444f45ed46eca18
Zargham Khan <zarghamkhan980@gmail.com>
2023-04-16 18:51:50 +0200

    rename stuff

--- src/shared/abstract.exception.ts
+++ src/shared/abstract.exception.ts
@@ -2,15 +2,15 @@ export abstract class AbstractException extends Error {
   abstract _tag: string;
   abstract _label: string;
 
-  constructor(_originalError: unknown) {
-    if (_originalError instanceof Error) {
-      super(_originalError.message);
-      this.stack = _originalError.stack;
-      this.name = _originalError.name;
+  constructor(error: unknown) {
+    if (error instanceof Error) {
+      super(error.message);
+      this.stack = error.stack;
+      this.name = error.name;
     }
 
-    if (_originalError instanceof String) {
-      super(_originalError.toString());
+    if (error instanceof String) {
+      super(error.toString());
     }
   }
 }
--- src/users/errors/cannot-create.exception.ts
+++ src/users/errors/cannot-create.exception.ts
@@ -4,7 +4,7 @@ import { AbstractException } from '../../shared/abstract.exception';
 export class CannotCreateException extends AbstractException {
   public _tag = 'CannotCreateException ';
   public _label = 'Can not create the user';
-  constructor(public _user: Partial<UserEntity>, _originalError: unknown) {
-    super(_originalError);
+  constructor(public _user: Partial<UserEntity>, error: unknown) {
+    super(error);
   }
 }
