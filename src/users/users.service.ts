d0c6ab6f33a8503342565146fb482c6f39880fb5
Patrick Roza <contact@patrickroza.com>
2023-04-18 13:58:52 +0200

    refactor: replace layers with context.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,30 +3,36 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UsersRepository } from './tags';
-import { Effect, Exit, Layer, Runtime, Scope } from '../prelude';
+import { Context, Effect, Runtime } from '../prelude';
 
 @Injectable()
 export class UsersService {
   // TODO: build the runtime asynchronously https://docs.nestjs.com/fundamentals/async-providers
-  // TODO: call cleanup https://github.com/nestjs/nest/issues/9497#issuecomment-1108703070
   // TODO: consider a global runtime, instead of one per module (users)
-  private readonly runtime = Effect.runSync(buildRuntime(Layer.succeed(UsersRepository, this.usersRepository)));
-  readonly runPromise = Runtime.runPromise(this.runtime.runtime);
+  private readonly runtime = Effect.runSync(
+    buildRuntimeFromContext(Context.make(UsersRepository, this.usersRepository)),
+  );
+  readonly runPromise = Runtime.runPromise(this.runtime);
   constructor(
     @InjectRepository(UserEntity)
     private usersRepository: Repository<UserEntity>,
   ) {}
 }
 
-function buildRuntime<R, E, A>(layer: Layer.Layer<R, E, A>) {
-  return Effect.gen(function* ($) {
-    const scope = yield* $(Scope.make());
-    const env = yield* $(Layer.buildWithScope(layer, scope));
-    const runtime = yield* $(Effect.provideContext(Effect.runtime<A>(), env));
+// // TODO: call cleanup https://github.com/nestjs/nest/issues/9497#issuecomment-1108703070
+// function buildRuntimeFromLayer<R, E, A>(layer: Layer.Layer<R, E, A>) {
+//   return Effect.gen(function* ($) {
+//     const scope = yield* $(Scope.make());
+//     const env = yield* $(Layer.buildWithScope(layer, scope));
+//     const runtime = yield* $(buildRuntimeFromContext(env));
 
-    return {
-      runtime,
-      clean: Scope.close(scope, Exit.unit()),
-    };
-  });
+//     return {
+//       runtime,
+//       clean: Scope.close(scope, Exit.unit()),
+//     };
+//   });
+// }
+
+function buildRuntimeFromContext<A>(env: Context.Context<A>) {
+  return Effect.provideContext(Effect.runtime<A>(), env);
 }
